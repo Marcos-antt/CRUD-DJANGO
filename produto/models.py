6ec5d049c04d63f6e24d66f5fdcf5c7b35e5dde6
Marcos-antt <62682507+Marcos-antt@users.noreply.github.com>
2023-02-24 11:14:53 -0300

    Update models.py

--- produto/models.py
+++ produto/models.py
@@ -1,10 +1,7 @@
 from asyncio.windows_events import NULL
 from django.db import models
 
-# Create your models here.
-#makemigrations no console, joga todas as classes de todos os app de cada arquivo models para pasta migrate,
-#001_initial.py referente ao app
-#migrate sobe o arquivo 0001_initial para o banco desejado.
+
 
 class Produto(models.Model):                                     
     nome = models.CharField(max_length=100)                     
