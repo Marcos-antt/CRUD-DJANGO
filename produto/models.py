77ec992e8b6ea14fc23d1cc6aa7f12c36df3c751
Marcos-antt <62682507+Marcos-antt@users.noreply.github.com>
2023-02-24 11:13:30 -0300

    Update models.py

--- produto/models.py
+++ produto/models.py
@@ -6,9 +6,9 @@ from django.db import models
 #001_initial.py referente ao app
 #migrate sobe o arquivo 0001_initial para o banco desejado.
 
-class Produto(models.Model):                                     #cada classe uma tabale no banco
-    nome = models.CharField(max_length=100)                      #cada campo uma coluna na tabela
-    quantidade = models.IntegerField()                           #integerFiel definir valor inteiro 
+class Produto(models.Model):                                     
+    nome = models.CharField(max_length=100)                     
+    quantidade = models.IntegerField()                     
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     imagem = models.CharField(max_length=100)
     Status = models.BooleanField()
